a1c4cf35c1beff2f2d21fc29fb3771b75d53f483
test <test@neto.com>
2023-02-14 12:40:07 +0300

    первый комит

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+
+class Load_yadisk:
+
+    def __init__(self, token):
+        self.token = token
+
+
+if __name__ == '__main__':
+    token = 'проверка'
+    a = Load_yadisk(token)
+    print(a)
+
+
+
+
+
+
+
+
